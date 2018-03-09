28259efc3b1c2e851328ddfe80882b5a75be4592
Vinod Pawar <droidvinod@gmail.com>
2018-03-09 23:39:46 +0530

    Deleted cricket.c and added Python code cricket.py

--- cricket.py
+++ cricket.py
@@ -12,44 +12,45 @@ batsmanRemaining = 9
 
 for i in range(len(inputScore)):
 
-#extras-bye
-if inputScore[i] == "b":
-extras = extras + 1
+	#extras-bye
+	if inputScore[i] == "b":
+		extras = extras + 1
 
-#extras-wide
-elif inputScore[i] == "w":
-extras = extras + 1
+	#extras-wide
+	elif inputScore[i] == "w":
+		extras = extras + 1
 
-#dot-ball
-elif inputScore[i] == ".":
-ballCount = ballCount + 1
-print "Dot ball"
+	#dot-ball
+	elif inputScore[i] == ".":
+		ballCount = ballCount + 1
+		print "Dot ball"
 
-#wicket
-elif inputScore[i] == "W":
-#playerOnStrike = playerOnGround + 1
-#batsmanRemaining = batsmanRemaining - 1
+	#wicket
+	elif inputScore[i] == "W":
+		#playerOnStrike = playerOnGround + 1
+		#batsmanRemaining = batsmanRemaining - 1
 
-P1 
+	#P1 
  
-#score
-elif type(inputScore[i]) == int:
-score = score + inputScore[i] 
+	#score
+	elif type(inputScore[i]) == int:
+		score = score + inputScore[i] 
 
-if inputScore[i] % 2 == 1:
-playerOnStrike = playerOnGround + 1
+	if inputScore[i] % 2 == 1:
+		playerOnStrike = playerOnGround + 1
 
-P1 = 
+	#P1 = 
 
-#ball-count
-ballCount = ballCount + 1
-print ballCount
+	#ball-count
+	ballCount = ballCount + 1
+	print ballCount
 
-if ballCount == 6:
-overs = overs + 1
-playerOnStrike = playerOnGround
-ballCount = 0
+	if ballCount == 6:
+		overs = overs + 1
+		playerOnStrike = playerOnGround
+		ballCount = 0
 
 score = score + extras
+
 print "Total overs:",overs,"overs and",ballCount, "ball"
 print "Total score: ",score
