659c0b7cfa9afe12eb77f2a67f1225a81d7a4d26
Vinod Pawar <droidvinod@gmail.com>
2018-03-12 22:20:20 +0530

    Some variable name changes

--- cricket.py
+++ cricket.py
@@ -1,16 +1,31 @@
 inputScore = [1,".",2,"w","W",6,".",2,"b",3,4]
-P1,P2,P3,P4,P5,P6,P7,P8,P9,P10 = 0
-playerOnGround = 1
-playerOnStrike = 0
-strike = P1
+
+players = ["P1","P2","P3","P4","P5","P6","P7","P8","P9","P10"]
+
+#player_score_initialization
+P1 = 0
+P2 = 0
+P3 = 0
+P4 = 0
+P5 = 0
+P6 = 0
+P7 = 0
+P8 = 0
+P9 = 0
+P10 = 0
+
+playerOnStrike = 1
+playerOnNonStrike = 2
+
 ballCount = 0
 score = 0
 extras = 0
 overs = 0
-batsmanRemaining = 9
+
 
 
 for i in range(len(inputScore)):
+
 
 	#extras-bye
 	if inputScore[i] == "b":
@@ -27,19 +42,21 @@ for i in range(len(inputScore)):
 
 	#wicket
 	elif inputScore[i] == "W":
-		#playerOnStrike = playerOnGround + 1
-		#batsmanRemaining = batsmanRemaining - 1
-
-	#P1 
+		if playerOnNonStrike > playerOnStrike:
+			playerOnStrike = playerOnNonStrike + 1
+		else:
+			playerOnStrike = playerOnStrike + 1
  
 	#score
+	#add score to player
 	elif type(inputScore[i]) == int:
 		score = score + inputScore[i] 
-
+		P1 = P1 + inputScore[i] 
+	#strike change on odd runs
 	if inputScore[i] % 2 == 1:
-		playerOnStrike = playerOnGround + 1
-
-	#P1 = 
+		temp = playerOnStrike
+		playerOnStrike = playerOnNonStrike
+		playerOnNonStrike = playerOnStrike
 
 	#ball-count
 	ballCount = ballCount + 1
@@ -47,10 +64,11 @@ for i in range(len(inputScore)):
 
 	if ballCount == 6:
 		overs = overs + 1
-		playerOnStrike = playerOnGround
+		temp = playerOnStrike
+		playerOnStrike = playerOnNonStrike
+		playerOnNonStrike = temp
 		ballCount = 0
 
 score = score + extras
-
 print "Total overs:",overs,"overs and",ballCount, "ball"
 print "Total score: ",score
