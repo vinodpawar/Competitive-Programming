d88c5f63bf3685c0379b86c93611b3836d97be0a
Vinod Pawar <droidvinod@gmail.com>
2018-06-21 20:06:58 +0530

    Output fixes

--- cryptography.py
+++ cryptography.py
@@ -1,9 +1,18 @@
 #A simple cryptography program that replaces the current letter with the fourth letter next to it.
-#Programmed in Python v2.7
+#Programmed in Python 2.7
 alphabates = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-inputName = "abcd"
+inputName = "abdcz"
+count = 0
 
 for i in range(len(inputName)):
-	for j in range(len(alphabates)):
-		if inputName[i] == alphabates[j]:
-			print alphabates[j+4]
+    for j in range(len(alphabates)):
+        if inputName[i] == alphabates[j]:
+            if inputName[i] == "w":
+                j = -4
+            if inputName[i] == "x":
+                j = -3
+            if inputName[i] == "y":
+                j = -2
+            if inputName[i] == "z":
+                j = -1
+            print alphabates[j+4]
