187e03d1b8c42658b25877f0e44115433623e2b4
Vinod Pawar <droidvinod@gmail.com>
2024-04-27 11:24:15 +0530

    Update atm.py

--- atm.py
+++ atm.py
@@ -1,10 +1,11 @@
-#ATM program to withdraw cash from ATM
-notesOf = [2000,500,100]
-amount = input("Enter amount: ")
+#The program takes an input amount from the user and calculates the number of currency 
+#notes (in denominations of 2000, 500, and 100) required to represent that amount.
+notesOf = [2000, 500, 100]
+amount = int(input("Enter amount: "))  #Converts input to integer
 notes = 0
 
 for i in range(len(notesOf)):
-	notes = amount / notesOf[i]
-	print notes, "notes of", notesOf[i]
-	amount = amount % notesOf[i]
+    notes = amount // notesOf[i]  #Uses integer division
+    print(notes, "notes of", notesOf[i])
+    amount = amount % notesOf[i]
 	
