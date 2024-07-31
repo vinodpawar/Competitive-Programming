0ad3166de247f530a1d7441bdcbb2e4678c1df4f
Vinod Pawar <droidvinod@gmail.com>
2024-07-31 11:37:05 +0530

    Java program that removes spaces between the String

---
+++ RemoveSpacesString.java
@@ -0,0 +1,12 @@
+public class RemoveSpacesString {
+
+	public static void main(String[] args) {
+		// Remove all the white spaces in a string
+
+	String str = "Hello this is test String";
+	
+	System.out.println(str.replaceAll(" ", ""));
+
+	}
+
+}
